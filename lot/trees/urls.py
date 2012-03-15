38c21cb0780c5a89c0dce030b9b4e36fb63b1095
Matthew Perry <mperry@ecotrust.org>
2012-03-15 16:34:52 -0700

    Remove some old cruft

--- lot/trees/urls.py
+++ lot/trees/urls.py
@@ -8,9 +8,4 @@ urlpatterns = patterns('',
         property_stand_list, name='trees-property_stand_list'),
     url(r'^upload_stands/$', 
         upload_stands, name='trees-upload_stands'),
-    #user requested analysis
-    #url(r'properties_list/$', properties_list, name='trees-properties_list'),
-    #url(r'^construct_stands/$', construct_stands, name='trees-construct_stands'),
-    #url(r'^stands/$', stands, name='trees-stands'),
-    #url(r'^demo/$', demo, name='twod-map2d'),
 )
