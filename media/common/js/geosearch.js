4b627d12305cff2d0e1a2018e64140f718a23102
Edwin Knuth <eknuth@ecotrust.org>
2012-04-12 11:00:39 -0700

    handling errors for geosearch

--- media/common/js/geosearch.js
+++ media/common/js/geosearch.js
@@ -2,6 +2,7 @@ var geosearch = function () {
     var self = this;
 
     self.searchTerm = ko.observable();
+    self.showError = ko.observable();
 
     self.geoSearch = function () {
 
@@ -11,20 +12,18 @@ var geosearch = function () {
             "type": "GET",
             "data": { "search": self.searchTerm },
             "success": function (response) {
-                // var popup = new OpenLayers.Popup("chicken",
-                //                        new OpenLayers.LonLat(response.center[0],response.center[1]),
-                //                        new OpenLayers.Size(100,100),
-                //                        self.searchTerm(),
-                //                        true);
-
-                // map.addPopup(popup);
-
                 var location = new OpenLayers.LonLat(response.center[0], response.center[1]),
                     size = new OpenLayers.Size(21,25),
                     offset = new OpenLayers.Pixel(-(size.w/2), -size.h),
                     icon = new OpenLayers.Icon('http://www.openlayers.org/dev/img/marker.png',size,offset);
                 app.markers.addMarker(new OpenLayers.Marker(location,icon.clone()));             
                 map.zoomToExtent(OpenLayers.Bounds.fromArray(response.extent));
+                self.showError(false);
+            },
+            "error": function (response) {
+                if (response.status === 404) {
+                    self.showError(true);
+                }
             }
         });
     };
