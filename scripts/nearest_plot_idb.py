7b5f6e49dfda2a041836588c276364845da729ab
Matthew Perry <mperry@ecotrust.org>
2012-12-17 16:58:57 -0800

    refactor to support more flexible argument passing, uncertainly measure.

--- scripts/nearest_plot_idb.py
+++ scripts/nearest_plot_idb.py
@@ -22,36 +22,43 @@ from trees.models import IdbSummary
 from trees.utils import angular_diff
 from scipy.spatial import KDTree
 from django.db.models import Min, Max
+import math
 
 
-def nearest_plot(categories, input_params, weight_dict):
+def nearest_plot(categories, input_params, weight_dict, k=5):
     search_params = input_params.copy()
     keys = search_params.keys()
-
     for sp in keys:
-        if sp not in ['aspect', 'geographic']:
-            categories[sp+"__isnull"] = False
+        categories[sp+"__isnull"] = False
 
     EQD = 3786 # World Equidistant Cylindrical (Sphere) 
-    stand_centroid = GEOSGeometry('SRID=4326;POINT(%f %f)' % search_params['geographic'])
+    stand_centroid = GEOSGeometry('SRID=4326;POINT(%f %f)' % (input_params['longitude_fuzz'], input_params['latitude_fuzz']))
     stand_centroid.transform(EQD)
 
     def plot_attrs(ps, keys):
         vals = []
         for attr in keys:
-            if attr == 'aspect':
-                angle = angular_diff(ps.calc_aspect, input_params['aspect'])
-                vals.append(angle)
-                search_params['aspect'] = 0 # anglular difference to self is 0
-            elif attr == 'geographic':
-                plot_centroid = GEOSGeometry('SRID=4326;POINT(%f %f)' % (ps.longitude_fuzz, ps.latitude_fuzz))
-                plot_centroid.transform(EQD)
-                distance = stand_centroid.distance(plot_centroid)
-                vals.append(distance)
-                search_params['geographic'] = 0 # distance to self is 0
-            else:
-                vals.append(ps.__dict__[attr])
+            vals.append(ps.__dict__[attr])
+
+        # an additional special case
+        angle = angular_diff(ps.calc_aspect, input_params['calc_aspect'])
+        vals.append(angle)
+        search_params['_aspect'] = 0 # anglular difference to self is 0
+
+        # Deal with latlon, another special case
+        plot_centroid = GEOSGeometry('SRID=4326;POINT(%f %f)' % (ps.longitude_fuzz, ps.latitude_fuzz))
+        plot_centroid.transform(EQD)
+        distance = stand_centroid.distance(plot_centroid)
+        vals.append(distance)
+        search_params['_geographic'] = 0 # distance to self is 0
         return vals
+
+    plotsummaries = list(IdbSummary.objects.filter(**categories))
+    ps_attr_list= [plot_attrs(ps, keys) for ps in plotsummaries]
+    
+    # include our additional special cases
+    keys.append('_aspect') 
+    keys.append('_geographic')
 
     weights = np.ones(len(keys))
     for i in range(len(keys)):
@@ -59,11 +66,9 @@ def nearest_plot(categories, input_params, weight_dict):
         if key in weight_dict:
             weights[i] = weight_dict[key]
 
-    plotsummaries = list(IdbSummary.objects.filter(**categories))
-    psar = [plot_attrs(ps, keys) for ps in plotsummaries]
     querypoint = np.array([float(search_params[attr]) for attr in keys])
 
-    rawpoints = np.array(psar) 
+    rawpoints = np.array(ps_attr_list) 
     num_candidates = len(plotsummaries)
     if num_candidates == 0:
         raise Exception("There are no candidate plots matching the categorical variables: %s" % categories)
@@ -79,62 +84,77 @@ def nearest_plot(categories, input_params, weight_dict):
     # Create tree and query it for nearest plot
     tree = KDTree(allpoints)
     querypoints = np.array([querypoint])
-    result = tree.query(querypoints, k=5)
+    result = tree.query(querypoints, k=k)
     distances = result[0][0]
     plots = result[1][0]
 
     top = zip(plots, distances)
-    return [plotsummaries[t[0]] for t in top], num_candidates
+    ps = []
+
+    xs = [100 * x for x in weights if x > 0]
+    squares = [x * x for x in xs]
+    print squares
+    print distances
+    max_dist = math.sqrt(sum(squares)) # the real max 
+    for t in top:
+        p = plotsummaries[t[0]]
+        p.__dict__['_kdtree_distance'] = t[1]
+        p.__dict__['_uncertainty'] = (t[1] / max_dist) * len(squares) # increase uncertainty as the number of variables goes up?
+        ps.append(p)
+    return ps, num_candidates
 
 
 def potential_minmax(categories, weight_dict):
     ps = IdbSummary.objects.filter(**categories)
-    keys = [k for k in weight_dict.keys() if k not in ['aspect','geographic']] # TODO 
+    keys = [k for k in weight_dict.keys() if not k.startswith("_")]
     args = [Min(k) for k in keys] + [Max(k) for k in keys] 
-    return ps.aggregate(*args) #Min('elev_ft'), Max('elev_ft'))
+    return ps.aggregate(*args)
 
 if __name__ == "__main__":
  
     categories = {
         'for_type_name': 'Douglas-fir', 
-        'for_type_secdry_name': 'Red alder', 
+        #'for_type_secdry_name': 'Red alder', 
     }
 
     input_params = {
-        'qmd_hwd_cm': 20,
+        'age_dom': 40,
         'elev_ft': 1279,
-        'aspect': 45, # special case
-        'geographic': (-123, 46), # special case
-        'calc_slope': 15,
-        'sumofba_ft2': 0,
-        'avgofba_ft2_ac': 0,
-        'avgofht_ft': 80,
-        'avgoftpa': 20,
-        'avgofdbh_in': 12,
+        'calc_slope': 25,
+        'calc_aspect': 225, 
+        'longitude_fuzz': -123.0, 
+        'latitude_fuzz': 43.0, 
     }
 
     # weighting
     # no key or 1 implies standard weighting 
     # > 1 implies more importance for that variable
     weight_dict = {
-        'qmd_hwd_cm': 5,
+        # special cases
+        'calc_aspect': 0, # ALWAYS ZERO
+        'longitude_fuzz': 0, # ALWAYS ZERO
+        'latitude_fuzz': 0, # ALWAYS ZERO
+        '_geographic': 1,
+        '_aspect': 1,
+
+        # Site attrs
         'elev_ft': 1,
-        'aspect': 1,
-        'geographic': 1,
         'calc_slope': 1,
-        'sumofba_ft2': 0,
-        'avgofba_ft2_ac': 0,
-        'avgofht_ft': 1,
-        'avgoftpa': 1,
-        'avgofdbh_in': 1,
+
+        # Optional
+        'age_dom': 1,
     } 
 
-    print potential_minmax(categories, weight_dict)
-    top, num_candidates = nearest_plot(categories, input_params, weight_dict)
+    pmm = potential_minmax(categories, weight_dict)
+    top, num_candidates = nearest_plot(categories, input_params, weight_dict, k=20)
 
-    print "Top 5 (out of", num_candidates, "candidates)"
+    print weight_dict
+    print 
+    print '\t'.join([x.ljust(12) for x in input_params.keys()])
+    print '\t'.join([str(x).ljust(12) for x in input_params.values()])
+    print 
+
+
+    print "Top matches (out of", num_candidates, "candidates)"
     for plot in top:
-        plot.__dict__['aspect'] = plot.calc_aspect
-        plot.__dict__['geographic'] = "%s %s" %  (plot.latitude_fuzz, plot.longitude_fuzz)
-        print ' '.join([x+":"+str(plot.__dict__[x]) for x in input_params.keys()])
-
+        print '\t'.join([str(plot.__dict__[x]).ljust(10) for x in input_params.keys()]), "\t", plot._kdtree_distance, "\t", plot._uncertainty
