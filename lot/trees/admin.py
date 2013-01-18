97cab18b93913794a82de6f6e75f368a839e1742
Matthew Perry <mperry@ecotrust.org>
2013-01-18 11:18:31 -0800

    increase weight of numeric stand attrs

--- lot/trees/admin.py
+++ lot/trees/admin.py
@@ -2,6 +2,6 @@ from trees.models import *
 from django.contrib import admin
  
 class PlotLookupAdmin(admin.ModelAdmin):
-    list_display = ('name', 'attr', 'units', 'weight')
+    list_display = ('name', 'attr', 'units', 'weight', 'type')
 
 admin.site.register(PlotLookup, PlotLookupAdmin)
