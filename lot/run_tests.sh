4eb250583033d117f43dffd1fc993bfa3b687aff
Matthew Perry <mperry@ecotrust.org>
2013-02-07 13:19:38 -0800

    script to run tests and publish coverage stats

---
+++ lot/run_tests.sh
@@ -0,0 +1,6 @@
+echo "manage.py test trees..."
+#coverage run manage.py test trees --noinput -v 2 2>&1 | tee ../mediaroot/tests/test_results.txt 
+echo "coverage html..."
+coverage html -d ../mediaroot/tests/test_coverage --include "trees/*"
+
+
