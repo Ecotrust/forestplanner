626a3987c2e7b126ce2673483c46c6db97b314ff
Matthew Perry <mperry@ecotrust.org>
2013-02-13 17:10:11 -0800

    include colorbrewer colors

--- lot/trees/models.py
+++ lot/trees/models.py
@@ -771,6 +771,16 @@ class IdbSummary(models.Model):
             }
         return summary
 
+# based on the "Paired" color palette from ColorBrewer
+colors_rgb = [
+    (166,206,227), (31,120,180), (178,223,138), 
+    (51,160,44), (251,154,153), (227,26,28), 
+    (253,191,111), (255,127,0), (202,178,214), 
+    (106,61,154), (255,255,153), (177,89,40)
+]
+color_hex = ['#%02x%02x%02x' % rgb_tuple for rgb_tuple in colors_rgb]
+
+
 @register
 class Strata(Feature):
     search_age = models.FloatField()
