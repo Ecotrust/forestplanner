f6ac47afda51427eed3d18991e1965c9737272bd
Matthew Perry <mperry@ecotrust.org>
2013-02-21 12:42:59 -0800

    autopep8

--- lot/trees/views.py
+++ lot/trees/views.py
@@ -4,25 +4,26 @@ from django.http import HttpResponse, HttpResponseBadRequest, HttpResponsePerman
 from django.template import RequestContext
 from django.shortcuts import render_to_response
 from madrona.common.utils import get_logger
-from geopy import geocoders  
+from geopy import geocoders
 from geopy.point import Point
 from trees.models import Stand
 import json
-import simplejson
 import os
 
 logger = get_logger()
+
 
 def manage_stands(request):
     '''
     Stand management view
     '''
-    return render_to_response('common/manage_stands.html', {}, 
-        context_instance=RequestContext(request))
+    return render_to_response('common/manage_stands.html', {},
+                              context_instance=RequestContext(request))
+
 
 def user_property_list(request):
     '''
-    Present list of properties for a given user 
+    Present list of properties for a given user
     '''
     # import down here to avoid circular dependency
     from trees.models import ForestProperty
@@ -44,6 +45,7 @@ def user_property_list(request):
 
     return HttpResponse(gj, mimetype='application/json', status=200)
 
+
 def upload_stands(request):
     '''
     Upload stands via OGR datasource
@@ -60,7 +62,7 @@ def upload_stands(request):
         form = UploadStandsForm(request.POST, request.FILES)
 
         if form.is_valid():
-            # confirm property 
+            # confirm property
             try:
                 prop_pk = request.POST['property_pk']
             except KeyError:
@@ -76,7 +78,8 @@ def upload_stands(request):
 
             # Save to disk
             f = request.FILES['ogrfile']
-            tempdir = os.path.join(tempfile.gettempdir(), request.user.username)
+            tempdir = os.path.join(
+                tempfile.gettempdir(), request.user.username)
             if not os.path.exists(tempdir):
                 os.makedirs(tempdir)
             ogr_path = os.path.join(tempdir, f.name)
@@ -84,14 +87,15 @@ def upload_stands(request):
             for chunk in f.chunks():
                 dest.write(chunk)
             dest.close()
-            
-            #if zipfile, unzip and change ogr_path
+
+            # if zipfile, unzip and change ogr_path
             if ogr_path.endswith('zip'):
                 shp_path = None
                 zf = zipfile.ZipFile(ogr_path)
                 for info in zf.infolist():
                     contents = zf.read(info.filename)
-                    shp_part = os.path.join(tempdir, info.filename.split(os.path.sep)[-1])
+                    shp_part = os.path.join(
+                        tempdir, info.filename.split(os.path.sep)[-1])
                     fout = open(shp_part, "wb")
                     fout.write(contents)
                     fout.close()
@@ -108,25 +112,29 @@ def upload_stands(request):
                 s = StandImporter(request.user)
 
                 if new_prop_name:
-                    s.import_ogr(ogr_path, new_property_name=new_prop_name, pre_impute=True) 
+                    s.import_ogr(ogr_path,
+                                 new_property_name=new_prop_name, pre_impute=True)
                 else:
                     try:
                         fp = ForestProperty.objects.get(pk=prop_pk)
                     except ForestProperty.DoesNotExist:
                         return HttpResponse('<p class="label label-important">Could not find forest property %s</p>' % prop_pk, status=404)
-                    s.import_ogr(ogr_path, forest_property=fp, pre_impute=True) 
+                    s.import_ogr(ogr_path, forest_property=fp, pre_impute=True)
             except Exception as err:
                 return HttpResponse('<p class="label label-important">Error importing stands:\n%s</p>' % (err,), status=500)
 
-            if not fp: 
-                fp = ForestProperty.objects.filter(name=new_prop_name).latest('date_modified')
-            res = HttpResponse(json.dumps({'X-Madrona-Select': fp.uid}), status=201, mimetype='application/json')
+            if not fp:
+                fp = ForestProperty.objects.filter(
+                    name=new_prop_name).latest('date_modified')
+            res = HttpResponse(json.dumps({'X-Madrona-Select': fp.uid}),
+                               status=201, mimetype='application/json')
             return res
     else:
         form = UploadStandsForm()
 
     return HttpResponse('<p class="label label-important">Error importing stands: Invalid form submission</p>', status=400)
-    #return render_to_response('upload.html', {'form': form}, status=status)
+    # return render_to_response('upload.html', {'form': form}, status=status)
+
 
 def geojson_forestproperty(request, instance):
     '''
@@ -134,35 +142,38 @@ def geojson_forestproperty(request, instance):
     '''
     return HttpResponse(instance.feature_set_geojson(), mimetype='application/json', status=200)
 
+
 def forestproperty_scenarios(request, instance):
     from trees.models import Scenario
 
-    # TODO secure this 
-    scenarios = Scenario.objects.filter(user=request.user, input_property=instance)
+    # TODO secure this
+    scenarios = Scenario.objects.filter(
+        user=request.user, input_property=instance)
     if len(scenarios) == 0:
-        s1 = Scenario(user=request.user, 
-                input_property=instance,
-                name="Grow Only",
-                description="No management activities; allow natural regeneration for entire time period.",
-                input_target_boardfeet=0,
-                input_target_carbon=True,
-                input_rxs={},
-                input_site_diversity = 10,
-                input_age_class = 10, 
-        )
+        s1 = Scenario(user=request.user,
+                      input_property=instance,
+                      name="Grow Only",
+                      description="No management activities; allow natural regeneration for entire time period.",
+                      input_target_boardfeet=0,
+                      input_target_carbon=True,
+                      input_rxs={},
+                      input_site_diversity=10,
+                      input_age_class=10,
+                      )
         s1.save()
-        s2 = Scenario(user=request.user, 
-                input_property=instance,
-                name="Conventional Even-Aged",
-                description="Even-aged management for timber. 40-year rotation clear cut.",
-                input_target_boardfeet=2000,
-                input_target_carbon=False,
-                input_rxs={},
-                input_site_diversity = 1,
-                input_age_class = 1, 
-        )
+        s2 = Scenario(user=request.user,
+                      input_property=instance,
+                      name="Conventional Even-Aged",
+                      description="Even-aged management for timber. 40-year rotation clear cut.",
+                      input_target_boardfeet=2000,
+                      input_target_carbon=False,
+                      input_rxs={},
+                      input_site_diversity=1,
+                      input_age_class=1,
+                      )
         s2.save()
-        scenarios = Scenario.objects.filter(user=request.user, input_property=instance)
+        scenarios = Scenario.objects.filter(
+            user=request.user, input_property=instance)
 
     if len(scenarios) == 0:
         # this should never happen
@@ -170,6 +181,7 @@ def forestproperty_scenarios(request, instance):
 
     res_json = json.dumps([x.property_level_dict for x in scenarios])
     return HttpResponse(res_json, mimetype='application/json', status=200)
+
 
 def geojson_features(request, instances):
     '''
@@ -189,7 +201,7 @@ def geosearch(request):
     """
     from geopy import distance
     if request.method != 'GET':
-        return HttpResponse('Invalid http method; use GET', status=405)        
+        return HttpResponse('Invalid http method; use GET', status=405)
 
     try:
         txt = unicode(request.GET['search'])
@@ -199,7 +211,7 @@ def geosearch(request):
     searchtype = lat = lon = None
     place = txt
     try:
-        p = Point(txt) 
+        p = Point(txt)
         lat, lon, altitude = p
         searchtype = 'coordinates'
     except:
@@ -227,7 +239,7 @@ def geosearch(request):
     if not searchtype or not lat or not lon:  # try another geocoder
         g = geocoders.GeoNames()
         try:
-            place, latlon = g.geocode(txt)  
+            place, latlon = g.geocode(txt)
             lat = latlon[0]
             lon = latlon[1]
             searchtype = 'geocoded_geonames'
@@ -241,10 +253,10 @@ def geosearch(request):
         extent = cntrbuf.extent
         loc = {
             'status': 'ok',
-            'search': txt, 
+            'search': txt,
             'place': place,
-            'type': searchtype, 
-            'extent': extent, 
+            'type': searchtype,
+            'extent': extent,
             'latlon': [lat, lon],
             'center': (cntr[0], cntr[1]),
         }
@@ -253,13 +265,14 @@ def geosearch(request):
     else:
         loc = {
             'status': 'failed',
-            'search': txt, 
-            'type': None, 
-            'extent': None, 
+            'search': txt,
+            'type': None,
+            'extent': None,
             'center': None,
         }
         json_loc = json.dumps(loc)
         return HttpResponse(json_loc, mimetype='application/json', status=404)
+
 
 def potential_minmax(request):
     from trees.utils import potential_minmax as _pmm
@@ -274,10 +287,12 @@ def potential_minmax(request):
     jpmm = json.dumps(pmm)
     return HttpResponse(jpmm, mimetype='application/json', status=200)
 
+
 def svs_image(request, gnn):
-    imgs = ["/media/img/svs_sample/svs%d.png" % x for x in range(1,7)]
+    imgs = ["/media/img/svs_sample/svs%d.png" % x for x in range(1, 7)]
     idx = int(gnn) % len(imgs)
     return HttpResponsePermanentRedirect(imgs[idx])
+
 
 def stand_list_nn(request):
     from plots import get_nearest_neighbors
@@ -330,12 +345,13 @@ def stand_list_nn(request):
 
     out = []
     out.append("Searching for:\n  ")
-    out.extend("%s, %s to %s in, %s tpa, %d ba est" % s for s in mod_stand_list)
+    out.extend(
+        "%s, %s to %s in, %s tpa, %d ba est" % s for s in mod_stand_list)
     out.append("   (Total basal area: %d ft2/ac)" % total_ba)
     out.append("\n")
 
-
-    ps, num_candidates = get_nearest_neighbors(site_cond, mod_stand_list, weight_dict, k=10)
+    ps, num_candidates = get_nearest_neighbors(
+        site_cond, mod_stand_list, weight_dict, k=10)
 
     stand_list_json = json.dumps(stand_list)
     site_cond_json = json.dumps(site_cond)
@@ -364,16 +380,18 @@ def stand_list_nn(request):
             <td><div style='background-color:red; margin:0px; color:red; display: inline-block; width:%dpx;'>_</div>%s</td>\
             <td>%s</td>\
             </tr>" % (pseries.name,
-                pseries['_certainty'] * 100,
-                pseries['PLOT_BA'],
-                pseries['TOTAL_BA'], pseries['TOTAL_BA'],
-                pseries['PLOT_BA'] - (pseries['TOTAL_BA'] + pseries['NONSPEC_BA']), pseries['PLOT_BA'] - (pseries['TOTAL_BA'] + pseries['NONSPEC_BA']),
-                pseries['NONSPEC_BA'], pseries['NONSPEC_BA'],
-                pseries['age_dom'],
-                )
-            )
+                      pseries['_certainty'] * 100,
+                      pseries['PLOT_BA'],
+                      pseries['TOTAL_BA'], pseries['TOTAL_BA'],
+                      pseries['PLOT_BA'] - (pseries['TOTAL_BA'] + pseries['NONSPEC_BA']), pseries['PLOT_BA'] - (
+                      pseries['TOTAL_BA'] + pseries['NONSPEC_BA']),
+                      pseries['NONSPEC_BA'], pseries['NONSPEC_BA'],
+                      pseries['age_dom'],
+                      )
+        )
     out.append("</table>")
     return render_to_response("trees/stand_list_nn.html", locals())
+
 
 def add_stands_to_strata(request, instance):
     from madrona.features.views import get_object_for_editing
@@ -388,11 +406,13 @@ def add_stands_to_strata(request, instance):
     instance.save()
     return HttpResponse("Stands %r added to %s" % (stands, instance.uid), mimetype='text/html', status=200)
 
+
 def strata_list(request, property_uid):
     from madrona.features.views import get_object_for_viewing
     from trees.models import ForestProperty, Strata
-    fprop = get_object_for_viewing(request, property_uid, target_klass=ForestProperty)
+    fprop = get_object_for_viewing(
+        request, property_uid, target_klass=ForestProperty)
     if isinstance(fprop, HttpResponse):
-        return fprop 
+        return fprop
     slist = fprop.feature_set(feature_classes=[Strata])
     return HttpResponse(json.dumps([x._dict for x in slist]), mimetype="text/javascript")
