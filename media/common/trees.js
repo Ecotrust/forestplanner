f39a7ed35aa7e022d317cb8d0da5690a8d14dd94
Matthew Perry <mperry@ecotrust.org>
2013-03-22 12:42:03 -0700

    fixture for layers

--- media/common/trees.js
+++ media/common/trees.js
@@ -7,93 +7,16 @@ var steps = [undefined]; // start with a blank to make it 1-indexed
 function init() {
     map = new OpenLayers.Map(); 
 
-    /*  TODO bring into layer manager
-    var gphy = new OpenLayers.Layer.Google( "Google Physical", {type: google.maps.MapTypeId.TERRAIN});
-    var ghyb = new OpenLayers.Layer.Google( "Google Hybrid", {type: google.maps.MapTypeId.HYBRID, numZoomLevels: 21});
-    var gsat = new OpenLayers.Layer.Google( "Google Satellite", {type: google.maps.MapTypeId.SATELLITE, numZoomLevels: 22});
-
-    var arrayMapboxTerrain = [
-        "http://a.tiles.mapbox.com/v3/examples.map-4l7djmvo/${z}/${x}/${y}.jpg",
-        "http://b.tiles.mapbox.com/v3/examples.map-4l7djmvo/${z}/${x}/${y}.jpg",
-        "http://c.tiles.mapbox.com/v3/examples.map-4l7djmvo/${z}/${x}/${y}.jpg"];
-
-    var arrayAerial = [
-        "http://oatile1.mqcdn.com/tiles/1.0.0/sat/${z}/${x}/${y}.jpg",
-        "http://oatile2.mqcdn.com/tiles/1.0.0/sat/${z}/${x}/${y}.jpg",
-        "http://oatile3.mqcdn.com/tiles/1.0.0/sat/${z}/${x}/${y}.jpg",
-        "http://oatile4.mqcdn.com/tiles/1.0.0/sat/${z}/${x}/${y}.jpg"];
-
-    var arrayOSM = [
-        "http://otile1.mqcdn.com/tiles/1.0.0/osm/${z}/${x}/${y}.jpg",
-        "http://otile2.mqcdn.com/tiles/1.0.0/osm/${z}/${x}/${y}.jpg",
-        "http://otile3.mqcdn.com/tiles/1.0.0/osm/${z}/${x}/${y}.jpg",
-        "http://otile4.mqcdn.com/tiles/1.0.0/osm/${z}/${x}/${y}.jpg"];
-
-    var baseAerial = new OpenLayers.Layer.OSM("MapQuest Open Aerial", arrayAerial, {attribution:"MapQuest"});
-    var baseOSM = new OpenLayers.Layer.OSM("Mapbox OSM Terrain", arrayMapboxTerrain, {attribution:"Mapbox"});
-    var nhd = new OpenLayers.Layer.XYZ( "Streams",
-        "http://watersgeo.epa.gov/ARCGIS/REST/services/OW/NHD_Med_Detailed_WMERC/MapServer/tile/${z}/${y}/${x}",
-        {sphericalMercator: true, isBaseLayer: false, visibility: false, opacity: 0.75, attribution:"US EPA Office of Water"} 
-    );
-    var huc = new OpenLayers.Layer.XYZ( "Watershed Boundaries",
-        "http://watersgeo.epa.gov/ArcGIS/rest/services/OW/WBD_WMERC/MapServer/tile/${z}/${y}/${x}",
-        {sphericalMercator: true, isBaseLayer: false, visibility: false, attribution:"US EPA Office of Water"} 
-    );
-    var soils = new OpenLayers.Layer.XYZ( "Soil Survey",
-        "http://server.arcgisonline.com/ArcGIS/rest/services/Specialty/Soil_Survey_Map/MapServer/tile/${z}/${y}/${x}",
-        {sphericalMercator: true, 
-         isBaseLayer: false, 
-         visibility: false, 
-         opacity: 0.75,
-         attribution: "ESRI, USDA Natural Resources Conservation Service"} 
-    );
     var bingApiKey = "AhYe6O-7ejQ1fsFbztwu7PScwp2b1U1vM47kArB_8P2bZ0jiyJua2ssOLrU4pH70";
-    var road = new OpenLayers.Layer.Bing({
-        name: "Bing Road",
-        key: bingApiKey,
-        type: "Road"
-    });
     var hybrid = new OpenLayers.Layer.Bing({
         name: "Bing Hybrid",
         key: bingApiKey,
         type: "AerialWithLabels"
     });
-    var aerial = new OpenLayers.Layer.Bing({
-        name: "Bing Aerial",
-        key: bingApiKey,
-        type: "Aerial"
-    });
-
-    // map.addLayers([hybrid, road]);
-    // map.addLayer(baseOSM);
-    // map.addLayers([ghyb, gphy]);
-    // map.addLayer(baseAerial);
-    // map.addLayer(nhd);
-    // map.addLayer(huc);
-    // map.addLayer(soils);
-
-    // soils.events.register('visibilitychanged', soils, function(evt) {
-    //     if (soils.visibility) {
-    //         $('.layersDiv').append('<div id="soil-legend" style="text-align:center;"><img src="/media/img/soil_legend.png"><br><a target="_blank" href="http://goto.arcgisonline.com/maps/Specialty/Soil_Survey_Map">more info</a></div>');
-    //     } else {
-    //         $('#soil-legend').remove();
-    //     }
-    // });
-*/
-
-    var esri_base = new OpenLayers.Layer.XYZ( "ESRI Topo Maps",
-        //"http://server.arcgisonline.com/ArcGIS/rest/services/USA_Topo_Maps/MapServer/tile/${z}/${y}/${x}",
-        "http://server.arcgisonline.com/ArcGIS/rest/services/NatGeo_World_Map/MapServer/tile/${z}/${y}/${x}",
-        {
-            sphericalMercator: true,
-            opacity: 0.35,
-            attribution: "ESRI, (c) 2011 National Geographic Society, I-Cubed"
-        } 
-    );
-    map.addLayer(esri_base);
+    map.addLayer(hybrid);
 
     // for (var i=map.layers.length-1; i>=0; --i) {
-    //     map.layers[i].animationEnabled = true;
+    //      map.layers[i].animationEnabled = true;
     // }
 
     app.markers = new OpenLayers.Layer.Markers("search", {displayInLayerSwitcher: false});               
