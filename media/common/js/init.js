29e57ce76a396a00d1c007dd4173e6b85c5771c6
Will Moore <wmoore@ecotrust.org>
2013-04-17 16:42:58 -0700

    Fixes #71. Default error handing for AJAX failures Making app.flash globally available. i Adding dismiss method to app.flash @TODO could stand to a) receive an object and b) be more knockoutish

--- media/common/js/init.js
+++ media/common/js/init.js
@@ -29,57 +29,58 @@ $(document).ready(function () {
     $(window).resize(app.onResize);
     app.onResize();
     map.zoomToExtent(OpenLayers.Bounds.fromArray([-13954802.50397, 5681411.4375898, -13527672.389972, 5939462.8450446]));
+	app.globalErrorHandling();
 
-    var options = {
-    beforeSubmit: function(formData, jqForm, options) {
-        var name, file;
-        $(formData).each(function () {
-            if (this.name === 'new_property_name') {
-                    name = this.value;
-            } else if (this.name === 'ogrfile') {
-                    file = this.value;
-            }
-        });
-        if (!name) {
-            $("#upload-propertyname-required").fadeIn();
-            return false;
-        }
-        if (!file) {
-            $("#upload-file-required").fadeIn();
-            return false;
-        }
+	var options = {
+		beforeSubmit: function(formData, jqForm, options) {
+			var name, file;
+			$(formData).each(function () {
+				if (this.name === 'new_property_name') {
+						name = this.value;
+				} else if (this.name === 'ogrfile') {
+						file = this.value;
+				}
+			});
+			if (!name) {
+				$("#upload-propertyname-required").fadeIn();
+				return false;
+			}
+			if (!file) {
+				$("#upload-file-required").fadeIn();
+				return false;
+			}
 
-        $("#uploadProgress").show();
-    },
-    uploadProgress: function(event, position, total, percentComplete) {
-        var percentVal = percentComplete + '%';
-        $("#uploadProgress .bar").css('width', percentVal);
-        if (percentComplete > 99) {
-            $("#uploadProgress").hide();
-            $("#uploadResponse").html('<p class="label label-info">Upload complete. Processing stand data...</p>');
-        }
-    },
-    complete: function(xhr) {
-        $("#uploadProgress").hide();
-        $("#uploadProgress .bar").css('width', '0%');
-    },
-    error: function(data, status, xhr) {
-        $("#uploadResponse").html(data.responseText);
-    },
-    success: function(data, status, xhr) {
-        if (xhr.status == 201) {
-            $("#uploadResponse").fadeOut();
-            $("#uploadResponse").html('<p class="label label-success">Success</p>');
-            $("#uploadResponse").fadeIn();
-            $('#uploadForm').clearForm();
-            var interval = setTimeout( function() {   
-                $("#uploadResponse").html('');
-                app.properties.viewModel.afterUploadSuccess(data);
-            }, 2000); 
-        }
-    }
+			$("#uploadProgress").show();
+		},
+		uploadProgress: function(event, position, total, percentComplete) {
+			var percentVal = percentComplete + '%';
+			$("#uploadProgress .bar").css('width', percentVal);
+			if (percentComplete > 99) {
+				$("#uploadProgress").hide();
+				$("#uploadResponse").html('<p class="label label-info">Upload complete. Processing stand data...</p>');
+			}
+		},
+		complete: function(xhr) {
+			$("#uploadProgress").hide();
+			$("#uploadProgress .bar").css('width', '0%');
+		},
+		error: function(data, status, xhr) {
+			$("#uploadResponse").html(data.responseText);
+		},
+		success: function(data, status, xhr) {
+			if (xhr.status == 201) {
+				$("#uploadResponse").fadeOut();
+				$("#uploadResponse").html('<p class="label label-success">Success</p>');
+				$("#uploadResponse").fadeIn();
+				$('#uploadForm').clearForm();
+				var interval = setTimeout( function() {   
+					$("#uploadResponse").html('');
+					app.properties.viewModel.afterUploadSuccess(data);
+				}, 2000); 
+			}
+		}
 
-    }; 
+	}; 
     $('#uploadForm').submit( function () {
         $(this).ajaxSubmit(options); 
         return false; 
@@ -91,3 +92,36 @@ $(document).ready(function () {
     });
 
 });
+
+app.globalErrorHandling = function () {
+	$('body').prepend('<div id="flash" class="alert fade in" style="display: none"><h4></h4><div></div><a class="close" href="#">&times;</a>');
+	app.$flash = $('#flash');
+
+	$( document ).ajaxError(function (event, request, settings, exception) {
+		app.flash(request.responseText);
+	});
+
+	app.$flash.find('.close').bind('click', app.flash.dismiss);
+	
+};
+
+//@TODO way to dismiss programatically once someone corrects the error -wm
+//@TODO should pass an object. it's 2013. -wm
+app.flash = function(message, header, flashType){
+	// bs: alert-error, alert-warning, 
+	var type = flashType || 'alert-error',
+		hdr = header || 'Oops',
+		msg = message || '';
+
+	app.$flash.addClass( type ).slideDown(200)
+		.find('div').text( msg ).end()
+		.find('h4').text( hdr );
+
+	// $(window).scrollTop(0);
+	$("html, body").animate({ scrollTop: 0 }, 300);
+
+};
+app.flash.dismiss = function () {
+	app.$flash.hide().removeClass('alert-error alert-warning alert-success');
+	app.$flash.find('div, h4').text('');
+};
