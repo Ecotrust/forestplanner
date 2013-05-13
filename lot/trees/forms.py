ffb2d5acd673c5947b5b017eeefc3bc539107484
Matthew Perry <mperry@ecotrust.org>
2013-05-13 11:52:17 -0700

    HiddenJSONField

--- lot/trees/forms.py
+++ lot/trees/forms.py
@@ -1,7 +1,10 @@
 from madrona.features.forms import FeatureForm, SpatialFeatureForm
 from django import forms
 from trees.models import Stand, Strata, ForestProperty, Scenario, ScenarioStand, MyRx, Rx
-from madrona.analysistools.widgets import SliderWidget
+#from madrona.analysistools.widgets import SliderWidget
+from django.forms.util import flatatt
+from django.utils.safestring import mark_safe
+import json
 
 
 class StandForm(SpatialFeatureForm):
@@ -26,8 +29,27 @@ class StrataForm(FeatureForm):
         model = Strata
 
 
+class HiddenJSONInput(forms.HiddenInput):
+    """
+    Widget that properly escapes text as JSON
+    Allows the the brower to directly JSON.parse() it
+    i.e. returns
+        <input value="{\"key\": value}">
+    instead of
+        <input value="{u'key': value}">
+    """
+    def render(self, name, value, attrs=None):
+        if value is None:
+            value = ''
+        final_attrs = self.build_attrs(attrs, type=self.input_type, name=name)
+        # Only add the 'value' attribute if a value is non-empty.
+        if value != '':
+            final_attrs['value'] = json.dumps(value)
+        return mark_safe(u'<input%s />' % flatatt(final_attrs))
+
+
 class ScenarioForm(FeatureForm):
-    input_rxs = forms.CharField(widget=forms.HiddenInput(), required=False)
+    input_rxs = forms.CharField(widget=HiddenJSONInput(), required=False)
     spatial_constraints = forms.CharField(widget=forms.HiddenInput(), required=False)
     input_property = forms.ModelChoiceField(
         label="", queryset=ForestProperty.objects.all(), widget=forms.HiddenInput())
@@ -45,7 +67,7 @@ class ScenarioForm(FeatureForm):
     input_target_boardfeet = forms.FloatField(widget=forms.HiddenInput(), label="Boardfeet", initial=0)
     input_age_class = forms.FloatField(widget=forms.HiddenInput(), label="Age class", initial=0)
     input_target_carbon = forms.BooleanField(widget=forms.HiddenInput(), label="Carbon", initial=True)
-    description = forms.CharField(widget=forms.Textarea(attrs={'cols': 40, 'rows': 2}), 
+    description = forms.CharField(widget=forms.Textarea(attrs={'cols': 40, 'rows': 4}), 
         label="Description", required=False)
 
     class Meta(FeatureForm.Meta):
