09e80bb6e5680d0fe416bfc84c77c1c387aa9e27
Will Moore <wmoore@ecotrust.org>
2013-05-28 12:21:45 -0700

    WIP on manage strata redesign

--- lot/trees/views.py
+++ lot/trees/views.py
@@ -101,7 +101,7 @@ def manage_strata(request, property_uid):
     Strata management view
     '''
     return render_to_response(
-        'common/manage_strata.html', {'property_id': property_uid},
+        'common/manage_strata_2.html', {'property_id': property_uid},
         context_instance=RequestContext(request))
 
 
