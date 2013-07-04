654916a5c009d1516db8881cacf3295f1319d580
Matthew Perry <mperry@ecotrust.org>
2013-07-03 18:18:38 -0700

    gather script adjustments

--- scripts/gather_subset/gather_10pct_step2.py
+++ scripts/gather_subset/gather_10pct_step2.py
@@ -1,3 +1,14 @@
+
+CERTAINTY_THRESHOLD = 0.85
+COUNT_THRESHOLD = 2
+PCT = .60
+
+# CERTAINTY_THRESHOLD = 0.90
+# COUNT_THRESHOLD = 1
+# PCT = 0.80
+# This subset of plots represents at least 80% of the plots that match w/ 90% certainty
+#  to at least 1 plot other than itself.
+
 counts = {
     'PN': {},
     'EC': {},
@@ -9,10 +20,21 @@ counts = {
 
 for line in open("matches2.csv").readlines()[1:]:
     items = line.strip().split(',')
-    matched_cond = items[2]
-    var = items[1]
+    try:
+        matched_cond = items[2]
+        var = items[1]
+        certainty = float(items[3])
+    except (ValueError, IndexError):
+        continue
 
-    dd = counts[var]
+    if certainty < CERTAINTY_THRESHOLD:
+        continue
+
+    try:
+        dd = counts[var]
+    except KeyError:
+        continue
+
     if matched_cond in dd.keys():
         dd[matched_cond] += 1
     else:
@@ -23,15 +45,15 @@ import math
 for var in counts.keys():
     x = counts[var]
     total = sum([d[1] for d in x.items()])
-    subset_hits = int(math.ceil(total * 0.50))  #
+    subset_hits = int(math.ceil(total * PCT))  #
     sorted_x = sorted(x.iteritems(), key=operator.itemgetter(1), reverse=True)
 
     tally = 0
     break_on_y1_change = False
     prev_y1 = 0
     for y in sorted_x:
-        # if y[1] <= 2:  # only one or two matches? don't run it
-        #     continue
+        if y[1] < COUNT_THRESHOLD:
+            continue
 
         if y[1] != prev_y1 and break_on_y1_change:
             break
