193ae98a1c8efe9f13996f846d1a14556e5ab1f6
Matthew Perry <mperry@ecotrust.org>
2013-11-25 12:22:49 -0800

    add script to test smtp server

---
+++ scripts/test_email.py
@@ -0,0 +1,22 @@
+import sys
+from django.core.management import setup_environ
+appdir = '/usr/local/apps/land_owner_tools/lot'
+sys.path.append(appdir)
+import settings
+setup_environ(settings)
+##############################
+from django.core import mail
+
+print "Sending mail to mperry@ecotrust.org...",
+mail.send_mail('Testing 123', 'test email ... Here is the message.',
+    'forestplanner@ecotrust.org', ['mperry@ecotrust.org'],
+    fail_silently=False)
+print "DONE"
+
+print "Sending mail to perrygeo@gmail.com...\n"
+mail.send_mail('Testing 123', 'test email ... Here is the message.',
+    'forestplanner@ecotrust.org', ['perrygeo@gmail.com'],
+    fail_silently=False)
+print "DONE"
+
+print "Everything works..."
