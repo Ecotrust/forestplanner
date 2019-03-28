1f94fb01408637530a839fa059c66be1eb9ebae4
David Pollard <pollardld@gmail.com>
2019-03-28 14:07:54 -0700

    Add templates and static files

--- lot/discovery/urls.py
+++ lot/discovery/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('documentation/', views.documentation, name='documentation'),
+    path('landing/', views.landing, name='landing'),
+    path('stand_profile/', views.stand_profile, name='stand_profile'),
 ]
--- lot/discovery/views.py
+++ lot/discovery/views.py
@@ -3,6 +3,28 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
-
+# Will probably need to edit this view
+# TODO: need to discuss what to do about static content in base.html; see see template comments
 def index(request):
-    return HttpResponse("What a Discovery")
+    context = {}
+    return render(request, 'discovery/common/base.html', context)
+
+# landing page for non logged in users
+def landing(request):
+    context = {}
+    return render(request, 'discovery/landing.html', context)
+
+# overwrite static content in lot app about.html
+def about(request):
+    context = {}
+    return render(request, 'discovery/common/about.html', context)
+
+# overwrite satic content in lot app documenation
+def documentation(request):
+    context = {}
+    return render(request, 'discovery/common/documentation.html', context)
+
+# support new layout for user stand/properties profiles
+def stand_profile(request):
+    context = {}
+    return render(request, 'discovery/stand_profile.html', context)
