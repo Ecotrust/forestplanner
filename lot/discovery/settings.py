e712ff7756cc7ae3a2fbd280795b58228c2b950f
Ryan Hodges <ryan.d.hodges@gmail.com>
2019-04-30 17:18:57 -0700

    adding email settings

--- lot/discovery/settings.py
+++ lot/discovery/settings.py
@@ -1,3 +1,7 @@
+EMAIL_HOST = 'localhost'
+EMAIL_PORT = 25
+EMAIL_HOST_USER = 'noreply@forestdiscovery.ecotrust.org'
+
 DEFAULT_STAND_SPLASH = "/static/discovery/img/Evergreen_(PSF).png"
 DEFAULT_STAND_IMAGE = "/static/discovery/img/Trees_icon_wide.png"
 
