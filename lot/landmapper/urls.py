449f43125cc6af9a8795a35d6c10a62a7b007609
David Pollard <pollardld@gmail.com>
2020-05-01 11:41:15 -0700

    Views testing

--- lot/landmapper/urls.py
+++ lot/landmapper/urls.py
@@ -1,7 +1,10 @@
-from django.urls import include, re_path
+from django.urls import include, re_path, path
 from landmapper.views import *
 
 urlpatterns = [
     re_path(r'',
         home, name='landmapper-home'),
+    path('', index, name="index"),
+    path('identify/', identify, name="identify"),
+    path('report/', report, name="report"),
 ]
--- lot/landmapper/views.py
+++ lot/landmapper/views.py
@@ -7,3 +7,39 @@ def home(request):
     '''
     # return render(request, 'landmapper/home.html', {})
     return render(request, 'landmapper/home.html', {})
+
+def index(request):
+    '''
+    Land Mapper: Index Page
+    '''
+    return render(request, 'landmapper/base.html', {})
+
+def identify(request):
+    '''
+    Land Mapper: Identify Pages
+    '''
+    return render(request, 'landmapper/base.html', {})
+
+def report(request):
+    '''
+    Land Mapper: Report Pages
+    '''
+    return render(request, 'landmapper/base.html', {})
+
+def create_property(request, taxlot_ids, property_name):
+    '''
+    Land Mapper: Create Property
+
+    TODO:
+        can a memory instance of feature be made as opposed to a database feature
+            meta of model (ref: madrona.features) to be inherited?
+            don't want this in a database
+            use a class (python class) as opposed to django model class?
+        add methods to class for
+            creating property
+            turn into shp
+            CreatePDF, ExportLayer, BuildLegend, BuildTables?
+        research caching approaches
+            django docs
+            django caching API
+    '''
