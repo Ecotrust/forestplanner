890e7e31695e4b774c1e272ec2ace21e1a08c572
Ryan Hodges <ryan.d.hodges@gmail.com>
2020-06-26 14:46:03 -0700

    change attribute font to be non-mono

--- lot/landmapper/settings.py
+++ lot/landmapper/settings.py
@@ -410,7 +410,8 @@ ATTRIBUTION_BOX_OUTLINE = None
 ATTRIBUTION_TEXT_COLOR = "black"
 # ATTRIBUTION_TEXT_FONT = 'Pillow/Tests/fonts/FreeMono.ttf'
 # default UBUNTU Font
-ATTRIBUTION_TEXT_FONT = '/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf'
+# ATTRIBUTION_TEXT_FONT = '/usr/share/fonts/truetype/dejavu/DejaVuSansMono.ttf'
+ATTRIBUTION_TEXT_FONT = '/usr/share/fonts/truetype/dejavu/DejaVuSans.ttf'
 ATTRIBUTION_TEXT_FONT_SIZE = 10
 ATTRIBUTION_TEXT_BUFFER = 3
 ATTRIBUTION_TEXT_LINE_SPACING = 1
