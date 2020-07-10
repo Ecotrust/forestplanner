7a55d9c62e0131db3ec8de14279bac19f6648901
David Pollard <pollardld@gmail.com>
2020-07-09 17:42:14 -0700

    Zoom and center map on address

--- lot/landmapper/static/landmapper/js/map.js
+++ lot/landmapper/static/landmapper/js/map.js
@@ -1,22 +1,14 @@
 var mapView = new ol.View({
-  center: ol.proj.fromLonLat([-122.41, 37.82]),
-  zoom: 4
+  center: ol.proj.fromLonLat([-122.41, 43.82]),
+  zoom: 6.5
 });
 
 var map = new ol.Map({
   target: 'map',
   layers: [
     new ol.layer.Tile({
-      // preload: Infinity,
-      title: 'Satellite Imagery',
-      source: new ol.source.XYZ({
-        url: 'https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}',
-        attributions: 'Sources: Esri, DigitalGlobe, Earthstar Geographics, CNES/Airbus DS, GeoEye, USDA FSA, USGS, Getmapping, Aerogrid, IGN, IGP, and the GIS User Community'
-      }),
-      name: 'Satellite',
-      type: 'base',
-      visible: false
-    }),
+      source: new ol.source.OSM()
+    })
   ],
   view: mapView
 });
