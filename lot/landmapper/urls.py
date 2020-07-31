099b9de65ab83f5cb46b9cf50efc3927c1d82129
Ryan Hodges <ryan.d.hodges@gmail.com>
2020-07-31 11:37:26 -0700

    merging mvc and fixing url parameter handoff

--- lot/landmapper/urls.py
+++ lot/landmapper/urls.py
@@ -8,7 +8,6 @@ urlpatterns = [
     path('', home, name="home"),
     path('/identify/', identify, name="identify"),
     # path('/report/', report, name="report"),
-    path('/create_property_id/', create_property_id, name='create_property_id'),
-    re_path(r'^report/((?P<property_id>\w+)/)?$', report, name='report'),
+    path('report/<str:cache_id>', report, name='report'),
     path('get_taxlot_json/', get_taxlot_json, name='get taxlot json'),
 ]
