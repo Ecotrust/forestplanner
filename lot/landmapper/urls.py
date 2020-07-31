47504100712fb29b5380fcaaf2647fb85443c348
David Pollard <pollardld@gmail.com>
2020-07-31 15:20:18 -0700

    Implement caching

--- lot/landmapper/urls.py
+++ lot/landmapper/urls.py
@@ -1,4 +1,5 @@
 from django.urls import include, re_path, path
+from django.views.decorators.cache import cache_page
 from landmapper.views import *
 
 urlpatterns = [
@@ -9,6 +10,6 @@ urlpatterns = [
     path('identify/', identify, name="identify"),
     # path('/report/', report, name="report"),
     path('create_property_id/', create_property_id, name='create_property_id'),
-    path('report/<str:property_id>', report, name='report'),
+    path('report/<str:property_id>', cache_page(60 * 60 * 24 * 7)(report), name='report'),
     path('get_taxlot_json/', get_taxlot_json, name='get taxlot json'),
 ]
