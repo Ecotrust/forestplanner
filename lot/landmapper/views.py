e72204dec037c12a836d79e9a8b5a5f8eb2e6be7
David Pollard <pollardld@gmail.com>
2020-08-03 15:25:13 -0700

    Report updates

--- lot/landmapper/views.py
+++ lot/landmapper/views.py
@@ -579,11 +579,7 @@ def create_property(taxlot_ids, property_name, user_id=False):
 def get_property_by_id(property_id):
     from django.core.cache import cache
     from django.contrib.sites import shortcuts
-    from django.contrib.gis.geos import GEOSGeometry, MultiPolygon
-    from django.contrib.auth.models import User
-    from .models import Taxlot, Property
-    import json
-
+    
     property = cache.get('%s' % property_id)
 
     if not property:
