0a5767b0991a1ebb274287e87e34e8c7e999d4e2
Ryan Hodges <ryan.d.hodges@gmail.com>
2020-08-11 14:26:38 -0700

    allowing domain-based branching for land owner tools installs

--- lot/lot/views.py
+++ lot/lot/views.py
@@ -1,9 +1,25 @@
 from django.conf import settings
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+# from landmapper.urls import home as landmapper_home
+
 
 def lot_landing_page(request):
     '''
     Land Owner Tools: Pick Your Tool
     '''
+
+    # get url
+    domain = request.get_host().split(':')[0]
+    # Check against DOMAIN_ROUTING
+    if domain in settings.DOMAIN_ROUTING.keys() and settings.DOMAIN_ROUTING[domain]:
+        # Redirect as appropriate
+        redirect_key = settings.DOMAIN_ROUTING[domain]
+        if redirect_key == 'landmapper':
+            return redirect('/landmapper')
+        if redirect_key == 'discovery':
+            return redirect('/discovery')
+        if redirect_key == 'forestplanner':
+            return redirect('/map')
+
     return render(request, 'lot/common/lot_selection.html', {})
