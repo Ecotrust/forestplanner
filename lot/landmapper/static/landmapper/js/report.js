5596c4dd7b79a5847c1fb303ef3442b147f5f4ae
David Pollard <pollardld@gmail.com>
2020-12-07 18:16:13 -0800

    Share button works for each map on report

--- lot/landmapper/static/landmapper/js/report.js
+++ lot/landmapper/static/landmapper/js/report.js
@@ -1,12 +1,14 @@
 var shareLinks = document.querySelectorAll('.copy-link');
 for (var i = 0; i < shareLinks.length; i++) {
   shareLinks[i].addEventListener('click', function(e) {
+    var mapDiv = e.target.closest('.section-report');
     var url = window.location.href
     const el = document.createElement('textarea');
-    el.value = url;
+    el.value = url.split('#')[0] + '#' + mapDiv.id;
     document.body.appendChild(el);
     el.select();
     document.execCommand('copy');
     document.body.removeChild(el);
+    window.alert('URL has been copied to your clipboard')
   })
 }
