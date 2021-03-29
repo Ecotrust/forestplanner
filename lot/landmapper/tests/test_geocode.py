1bc4f3379216613c67e6451769fcbd2804c5a78a
Ryan Hodges <ryan.d.hodges@gmail.com>
2021-03-29 15:30:24 -0700

    creating test for geocoder

---
+++ lot/landmapper/tests/test_geocode.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+from django.conf import settings
+
+class GeocodeTest(TestCase):
+    def setUp(self):
+        pass
+
+    def test_geocode_studyregion(self):
+        from landmapper.views import geocode
+        for teststring in ['toledo', 'glide', 'Beaver Marsh', 'steens', 'joseph', 'k falls']:
+            print('TESTING: %s...\n' % teststring)
+            result_coords = geocode(teststring)
+            self.assertTrue(result_coords[0] <= settings.STUDY_REGION['north'])
+            self.assertTrue(result_coords[0] >= settings.STUDY_REGION['south'])
+            self.assertTrue(result_coords[1] <= settings.STUDY_REGION['east'])
+            self.assertTrue(result_coords[1] >= settings.STUDY_REGION['west'])
+            
